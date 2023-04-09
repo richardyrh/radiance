5b6b2fef1489f5bd06692e51b4c83c81b86a7911
Hansung Kim <hansung_kim@berkeley.edu>
2023-04-09 14:53:02 -0700

    Placeholder for MemTraceLogger C++ code

---
+++ src/main/resources/csrc/SimMemTraceLogger.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <memory>
+#include <fstream>
+
+class MemTraceLogger;
+
+struct MemTraceLine {
+  bool valid = false;
+  long cycle = 0;
+  char loadstore[10];
+  int core_id = 0;
+  int lane_id = 0;
+  unsigned long address = 0;
+  unsigned long data = 0;
+  int data_size = 0;
+};
+
+class MemTraceLogger {
+public:
+  MemTraceLogger(const std::string &filename);
+  ~MemTraceLogger();
+  // void parse();
+  // MemTraceLine read_trace_at(const long cycle, const int lane_id);
+  // bool finished() const { return read_pos == trace.cend(); }
+
+  std::ifstream infile;
+  // std::vector<MemTraceLine> trace;
+  // std::vector<MemTraceLine>::const_iterator read_pos;
+};
+
+extern "C" void memtracelogger_init(const char *filename);
+extern "C" void memtracelogger_log(unsigned char *trace_log_ready);
